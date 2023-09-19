b23be2f30de65f5b1a0012f0fc9d97bc676aa4c6
Mari-Lew <mars.lewinski@gmail.com>
2023-09-19 11:22:00 -0500

    User input for login complete

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 // Imports
   import { StatusBar } from 'expo-status-bar';
-  import React from 'react';
+  import React, { useState } from 'react';
   import { View, Image, Text, ImageBackground, TouchableOpacity, Dimensions , TextInput, StyleSheet } from 'react-native';
   import LinearGradient from 'react-native-linear-gradient';
   import { buttons } from './Styles/buttons'
@@ -8,17 +8,22 @@
 export default function App() {
   // Variables and Such
     const { width, height } = Dimensions.get('window'); // want the dimenstions of the screen width and height
-    const [emailInput, setLoginText] = React.useState(''); // State to hold user input
-    const [passwordInput, setPasswordText] = React.useState(''); // State to hold user input
+    const [emailInput, setEmailText] = useState(''); // State to hold user input
+    const [passwordInput, setPasswordText] = useState(''); // State to hold user input
+
+  const handleEmailState = (text) => {
+    setEmailText(text)
+  }
+  const handlePasswordState = (text) => {
+    setPasswordText(text)
+  }
 
     const logIn = () => {
       // Handle the login logic here
-      console.log('Login Button Clicked');
+      console.log('Email: ', emailInput);
+      console.log('Password: ', passwordInput);
     };
 
-    const signUp = () => {
-      console.log('Sign Up Button Clicked');
-    }
 
   return (
     <View style={styles.container}>
@@ -31,13 +36,32 @@ export default function App() {
     { /**/}
 
     <View style={styles.overlay}>
-{/*LOGIN BUTTON*/}
+    {/*EMAIL INPUT*/}
     <View style={buttons.buttonContainer}>
+    
+    <TextInput
+        style={styles.input}
+        placeholder="Enter Email"
+        onChangeText={handleEmailState}
+        value={emailInput}
+        underlineColorAndroid="transparent"
+        placeholderTextColor="white"
+        color="white"
+      />
+
+<TextInput
+        style={styles.input}
+        placeholder="Enter Password"
+        secureTextEntry={true}
+        onChangeText={handlePasswordState}
+        value={passwordInput}
+        underlineColorAndroid="transparent"
+        placeholderTextColor="white"
+        color="white"
+      />
+
     <TouchableOpacity style={buttons.buttonStyle} onPress={logIn}>
-      <Text style={buttons.text}>Log in</Text>
-    </TouchableOpacity>
-    <TouchableOpacity style={buttons.buttonStyle} onPress={signUp}>
-      <Text style={buttons.text}>Sign Up</Text>
+      <Text style={buttons.text}>LOGIN</Text>
     </TouchableOpacity>
 
 
@@ -56,11 +80,29 @@ const styles = StyleSheet.create({
   background: {
     flex: 1, // Makes the image cover the entire screen
     resizeMode: 'cover', // Resizes the image to cover the container
+    
   },
   overlay: {
     flex: 1,
     backgroundColor: 'rgba(0, 0, 0, 0.3)', // You can add a semi-transparent overlay if needed
     justifyContent: 'center',
     alignItems: 'center',
+    
+  },
+  input: {
+    width: '70%',
+    padding: 10,
+    borderWidth: 1,
+    borderRadius: 100,
+    backgroundColor: 'rgba(255, 255, 255, 0.5)',
+    borderColor: 'rgba(255, 255, 255, 0.5)',
+    margin: 10,
+    alignItems: 'center',
+    textAlign: 'center',
+    fontSize: 20
+  },
+
+  verticalText: {
+    width: '100%'
   },
 });
--- Styles/buttons.js
+++ Styles/buttons.js
@@ -1,15 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { View, Image, Text, TouchableOpacity, TextInput, StyleSheet } from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
+import * as Font from 'expo-font';
 
 export const buttons = StyleSheet.create({
     buttonStyle: {
+      width: '80%',
       backgroundColor: '#fff', // background color
-      borderRadius: 80, // Rounded corners
-      paddingVertical: 16, // Vertical padding
-      paddingHorizontal: 92, // Horizontal padding
-      margin: 15,
+      borderRadius: 100, // Rounded corners
+      paddingVertical: 15, // Vertical padding
+      paddingHorizontal: 20, // Horizontal padding
+      margin: 10,
       alignItems: 'center', // Center text horizontally
       justifyContent: 'center', // Center text vertically
       shadowColor: '#000',
@@ -21,17 +23,27 @@ export const buttons = StyleSheet.create({
       shadowRadius: 2,
       elevation: 3
     },
+
     buttonContainer: {
       flexDirection: 'column', // Arrange buttons horizontally
       justifyContent: 'space-between', // Distribute space between buttons
-      width: '95%', // Adjust the width of the container
-    },
+      width: '100%', // Adjust the width of the container
+      position: 'absolute',
+      bottom: 100,
+      flex: 1,
+      justifyContent: 'center',
+      //padding: 10,
+      
+    }, 
     
     text: {
+      fontFamily: 'texgyreadventor-bold', // does not read right now
       color: '#000000', // text color
       fontSize: 20, // Text size
     }
     })
+
+
     //----------- Login stuff below until refactor --------------------------------- 
     const LoginButton = ({ onPress }) => {
     return (
